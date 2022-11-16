3860074e920c0a25d808b96c2b93a651a1601635
Jurriaan Buist <jurriaanbu@gmail.com>
2022-11-16 12:41:54 +0100

    Gave repo structure for pytest

--- hello.py
+++
@@ -1,5 +0,0 @@
-msg = "Hello World"
-print(msg)
-print("develop branch")
-print("new feature")
-print("some more changes")
---
+++ tests/test_main.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_hello(capfd):    
+    import hello
+    out, err = capfd.readouterr()
+    assert out == "Hello World!\n"    
+    
